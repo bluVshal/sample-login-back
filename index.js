02a94d94003fafd486b6973ed3753009b1708f9f
bluVshal <blu.vishal@gmail.com>
2024-07-19 18:35:26 +0400

    Modified Signup Process

--- index.js
+++ index.js
@@ -1,35 +1,58 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const cors = require('cors');
 const moment = require('moment');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
 
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 const db = mysql.createConnection({
     host:"localhost",
     user: "root",
-    password: "**********",
+    password: "jq88esfx",
     database: "sample_signup"
 });
 
-app.post("/signup", (req, res) => {
-    const sql = `INSERT INTO login VALUES (?)`;
-    const values = [
-        req.body.uname,
-        req.body.email,
-        req.body.password,
-        moment().format('YYYY-MM-DD HH:mm:ss'),
-        moment().format('YYYY-MM-DD HH:mm:ss'),
-        1
-    ];
+app.get("/", (req, res) => {
+    res.send("Listening");
+})
 
-    db.query(sql, [values], (err, data) => {
+app.post("/signup", jsonParser, (req, res) => {
+    const sqlSearch = `SELECT userId FROM login ORDER BY userId DESC LIMIT 1`;
+    const sql = `INSERT INTO login VALUES (?)`;
+    var latestUserID = 0;
+    
+    db.query(sqlSearch, (err, data) => {
         if(err){
-            return res.json(err);
-        }
-        return res.json(data);
-    })
+            console.log(err);
+        }       
+        console.log(data[0].userId);
+        latestUserID = parseInt(data[0].userId) + 1;
+
+        const values = [
+            latestUserID,
+            req.body.values.uname,
+            req.body.values.email,
+            req.body.values.password,
+            moment().format('YYYY-MM-DD HH:mm:ss'),
+            moment().format('YYYY-MM-DD HH:mm:ss'),
+            1
+        ];
+    
+        db.query(sql, [values], (err, data) => {
+            if(err){
+                return res.json(err);
+            }
+            return res.json(data);
+        });
+    });
 });
 
 app.listen(65319, () => {
